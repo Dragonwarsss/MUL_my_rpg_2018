31c6b8ba15e1629fe07f339a60917465bdbc5df9
sellali <mehdi.sellali@epitech.eu>
2019-04-30 03:53:40 +0200

    [FIX] map_editor bug filled both layer at the same time

--- bonus/map_editor/src/key_map.c
+++ bonus/map_editor/src/key_map.c
@@ -12,11 +12,11 @@ void key_map(sfEvent *event, edit_t *edit)
 {
     if (event->type == sfEvtKeyPressed) {
         if (event->key.code == sfKeyP && edit->ground == 0) {
-            edit->ground += 1;
+            edit->ground = 1;
             change_text(edit, "map2");
         }
         if (event->key.code == sfKeyM && edit->ground == 1) {
-            edit->ground -= 1;
+            edit->ground = 0;
             change_text(edit, "map1");
         }
     }
