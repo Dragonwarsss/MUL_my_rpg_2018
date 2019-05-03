71642c76da36cadc9554189a2f6afa3f27f450d3
sellali <mehdi.sellali@epitech.eu>
2019-05-03 04:55:12 +0200

    [FIX] map_display

--- src/display/display_game.c
+++ src/display/display_game.c
@@ -8,8 +8,14 @@
 #include "map.h"
 #include "my.h"
 
+void player_event(UNUSED game_t *game, UNUSED sfRenderWindow *window)
+{
+    interact(game->player, game->maps[game->map], game);
+}
+
 void display_game(game_t *game, sfRenderWindow *window)
 {
     display_map(game->maps[game->map], window);
     display_player(game->player, window);
+    player_event(game, window);
 }
