726580ca148ea6b7495c9b09e63123a460fe26d8
sellali <mehdi.sellali@epitech.eu>
2019-05-05 23:02:24 +0200

    [ADD] paths.h define paths to ressources

--- include/paths.h
+++ include/paths.h
@@ -8,12 +8,23 @@
 #ifndef PATHS_H_
 #define PATH_H_
 
-#define VALGRIND "ressources/asset/valgrind.png"
+#define VALGRIND "ressources/asset/weapons/valgrind.png"
 #define GDB "ressources/asset/gdb.png"
 #define ENNEMIES "ressources/asset/bug.jpg"
 #define VALGRINDLV1 "ressources/asset/weapons/valgrindlv1.png"
 #define VALGRINDLV2 "ressources/asset/weapons/valgrindlv2.png"
 #define VALGRINDLV3 "ressources/asset/weapons/valgrindlv3.png"
 #define FONT "ressources/font/font.ttf"
+#define PLAY "ressources/asset/menu/play.jpg"
+#define OPTIONS "ressources/asset/menu/options.jpg"
+#define QUIT "ressources/asset/menu/quit.jpg"
+#define TEXTBOX "ressources/asset/utils/text_box.png"
+#define TIMER "ressources/asset/utils/timer.png"
+#define TIMERFONT "ressources/asset/utils/timerfont.png"
+#define PRES "ressources/asset/pause/pause_res.png"
+#define PMENU "ressources/asset/pause/pause_menu.png"
+#define PQUIT "ressources/asset/pause/pause_quit.png"
+#define OPT_BUTTON "ressources/asset/menu/opt_music_icon.png"
+#define OPT_TEMP "ressources/asset/menu/opt_bg_template.png"
 
 #endif
